a562bb1f403d636a51f967d8a6fc84f03cbc84b9
Yuichiro Nakamine <e185726@ie.u-ryukyu.ac.jp>
2018-11-26 14:41:16 +0900

    コミットメッセージ

--- src/main/jp/ac/uryukyu/ie/e185726/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e185726/Enemy.java
@@ -46,9 +46,11 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+        if(!isDead()) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }
     }
 
     /**
