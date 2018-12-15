2b4f804c00e31f4851812758dfd9754b27f954f2
Yuichiro Nakamine <e185726@ie.u-ryukyu.ac.jp>
2018-12-15 20:56:32 +0900

    2番目のコミット

--- src/test/jp/ac/uryukyu/ie/e185726/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185726/EnemyTest.java
@@ -4,20 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
- public class EnemyTest {
+class EnemyTest {
     /**
      * 「死んでる状態では攻撃できない」ことを確認。
      */
     @Test
-     public void attack() {
+    void attack() {
         int heroHP = 10;
-        Hero hero = new Hero("テスト勇者", heroHP, 5);
-        Enemy enemy = new Enemy("テストスライム", 6, 3);
-        enemy.dead = true;
+        Hero hero = new Hero();
+        Enemy enemy = new Enemy();
+        enemy.setDead(true);
         for(int i=0; i<10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
-        assertEquals(heroHP, hero.hitPoint);
+        assertEquals(heroHP, hero.getHitpoint());
     }
 
 }
